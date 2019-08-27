4660cfbc7f916ccfd1577997995285efc7696006
Agnieszka Stankiewicz <agnieszka.stankiewicz77@gmail.com>
2019-08-27 23:12:01 +0200

    Dishwasher class implements iClean interface

---
+++ src/main/java/Smokozord/Dishwasher.java
@@ -0,0 +1,14 @@
+public class Dishwasher implements iClean {
+    
+    public void clean(){
+        system.out.println("I'm cleaning");
+    }
+
+    public void vaccum(){
+        system.out.println("I'm vaccuming");
+    }
+
+    public void iron(){
+        system.out.println("I'm ironing");
+    }
+}
